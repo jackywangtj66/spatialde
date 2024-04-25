38e597f9cecba345bc86574737934601d4c724a9
jackywangtj66 <jackywangtj66@gmail.com>
2024-04-25 19:29:23 +0800

    update

--- base.py
+++ base.py
@@ -34,6 +34,7 @@ class Kernel:
         self.l = l
         self.d = d
         self.A = []
+        self.cond_cov = []
         self._initialize()
     
     def _initialize(self):
@@ -65,12 +66,21 @@ class Kernel:
                 self.A.append(self.all_cov[np.ix_(self.ss_loc[i],loc)] @ u)
     
     def _init_base_cond_cov(self):
-        self.base_cond_cov = []
         for i,loc in enumerate(self.ds_loc):
             if len(loc) == 0:
-                self.base_cond_cov.append(self.all_cov[np.ix_(self.ss_loc[i],self.ss_loc[i])])
+                self.cond_cov.append(self.all_cov[np.ix_(self.ss_loc[i],self.ss_loc[i])])
             else:
-                self.base_cond_cov.append(self.all_cov[np.ix_(self.ss_loc[i],self.ss_loc[i])] - np.multiply(1/self.ds_eig[i][0],self.A[i])@self.A[i].T)
+                self.cond_cov.append(self.all_cov[np.ix_(self.ss_loc[i],self.ss_loc[i])] - np.multiply(1/self.ds_eig[i][0],self.A[i])@self.A[i].T)
+
+    def update_cond_cov(self,delta):
+        self.cond_cov_eig = []
+        for i in range(self.M):
+            if len(self.dependency[i]) == 0:
+                #self.cond_cov.append(self.kernel.base_cond_cov[i]+self.delta*np.eye(len(self.kernel.ss_loc[i])))
+                s,u = np.linalg.eigh(self.cond_cov[i]+delta*np.eye(len(self.ss_loc[i])))
+            else:
+                s,u = np.linalg.eigh(self.cond_cov[i]+delta*np.eye(len(self.ss_loc[i]))+delta*np.multiply(1/((self.ds_eig[i][0]+delta)*self.ds_eig[i][0]),self.A[i])@self.A[i].T)
+            self.cond_cov_eig.append((s,u))
 
 
 
@@ -78,6 +88,7 @@ class Gaussian:
     def __init__(self,kernel,mean=0,sigma_sq=1,delta=1) -> None:
         assert isinstance(kernel,Kernel)
         self.kernel = kernel
+        # mean should be N*1
         if mean.ndim == 1:
             self.mean = np.array(mean)[:,np.newaxis]
         elif mean.ndim == 0:
@@ -88,16 +99,14 @@ class Gaussian:
         self.delta = delta
         self.cond_eig = []
         self.temp = []
-        self.cond_cov = []
-    
-    def update_cond_conv(self):
+
+    def update_cond_mean(self,Y):
+        self.cond_dev = Y - self.mean
+        dev = Y - self.mean
         for i in range(self.kernel.M):
-            if len(self.kernel.dependency[i]) == 0:
-                #self.cond_cov.append(self.kernel.base_cond_cov[i]+self.delta*np.eye(len(self.kernel.ss_loc[i])))
-                s,u = np.linalg.eigh(self.kernel.base_cond_cov[i]+self.delta*np.eye(len(self.kernel.ss_loc[i])))
-            else:
-                s,u = np.linalg.eigh(self.kernel.base_cond_cov[i]+self.delta*np.eye(len(self.kernel.ss_loc[i]))+self.delta*np.multiply(1/((self.ds_eig[i][0]+self.delta)*self.ds_eig[i][0]),self.A[i])@self.A[i].T)
-            self.cond_conv.append((s,u))
+            if len(self.kernel.dependency[i]) > 0:
+                self.cond_dev[self.kernel.ss_loc[i],:] -= np.multiply(1/(self.kernel.ds_eig[i][0]+self.delta),self.kernel.A[i]) @ self.kernel.ds_eig[i][1].T @ dev[self.kernel.ds_loc[i],:]
+
 
     def ll_sep(self,Y):
         """
@@ -106,57 +115,13 @@ class Gaussian:
         N,G = Y.shape
         ll = np.log(2 * np.pi)*N + 2*np.log(self.sigma_sq)*N
         result = np.array([-0.5*ll]*G)
-        dev = Y - self.all_mean    #N*G
-        self.update_cond_conv()
+        self.kernel.update_cond_cov(self.delta)
+        self.update_cond_mean(Y)
         for i in range(self.M):
-            det = np.prod(self.cond_cov[i][0])
+            det = np.prod(self.kernel.cond_cov_eig[i][0])
             result += np.log(det)
-
-            
-            if len(self.dependency[i]) == 0:
-            
-                cond_dev = dev[self.ss_loc[i]]
-            else:
-                cond_dev = self.get_cond_dev(dev,i)
-            for g in range(G):
-                result[g] += -0.5* (cond_dev[:,g].T @ inv @ cond_dev[:,g])
-        return np.array(result)
-    
-    def _init_cond_cov(self):
-        eps = 1e-5
-
-        #start = time.time()
-        for i in range(self.kernel.M):
-            in_cov = self.kernel.all_cov[np.ix_(self.kernel.ss_loc[i],self.kernel.ss_loc[i])]    #m,m
-            if len(self.dependency[i]) == 0:
-                cond_cov = in_cov
-                self.base_cond_cov.append([])
-                self.A.append([])
-            else:
-                
-                self.A.append(self.all_cov[np.ix_(self.ss_loc[i],self.ds_loc[i])] @ self.kernel.ds_eig[i][1])
-                cond_cov = in_cov - np.multiply(self.A[i],self.kernel.ds_eig[i][0]) @ self.A[i].T              #Sigma_(m)(m)^\prime
-            cond_cov_inv = np.linalg.inv(cond_cov)
-            L = np.linalg.cholesky(cond_cov_inv)
-                # print(np.average((cond_cov_1-cond_cov)/cond_cov))
-                # np.savetxt("cond_cov_1_"+str(i),np.ones_like(in_cov)*self.ds_inv_sums[i]*np.average(self.ss_cov[i,self.dependency[i]])**2)
-                # np.savetxt("cond_cov_"+str(i),self.all_cov[np.ix_(self.ss_loc[i],self.ds_loc[i])] @ self.ds_inv[i] @ self.all_cov[np.ix_(self.ds_loc[i],self.ss_loc[i])])
-                # np.savetxt("in_cov_"+str(i),in_cov)
-            #treat cond_cov as positive definite 
-            self.cond_inv.append(np.linalg.inv(cond_cov))
-            #print(np.linalg.det(cond_cov))
-            #self.log_pdet.append(np.log(np.linalg.det(cond_cov)))
-            s, u = scipy.linalg.eigh(cond_cov)
-            #print(len(s[s<=0]))
-            d = s[s > eps]
-            s_pinv = _pinv_1d(s,eps)
-            # print(len(s_pinv[s_pinv<eps]),len(s_pinv[s_pinv<-eps]))
-            # print(s_pinv[s_pinv<eps])
-            # print("det:",np.sum(np.log(d)))
-            # self.cond_U.append(np.multiply(u, np.sqrt(s_pinv)))
-            # #print(np.isnan(self.cond_U).any())
-            
-            self.log_pdet.append(np.sum(np.log(d)))
-        #print("%.3f seconds" %(time.time()-start))
+            temp = self.cond_dev[self.kernel.ss_loc[i],:].T @ self.kernel.A[i]
+            result += np.sum(np.multiply(self.kernel.cond_cov_eig[i][0],np.square(temp)),axis=1)/self.sigma_sq
+        return result*-0.5
         
         
